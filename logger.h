26c21ad245396dccf6fdc7fab6c9980aaae016a9
Mateusz <01206777@pw.edu.pl>
2026-01-26 14:17:46 +0100

    Dodanie pliku logger.h

---
+++ logger.h
@@ -0,0 +1,10 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#include "pso.h" 
+
+void logger_init(const char *filename);
+
+void log_swarm(const char *filename, int iteration, int id, particle *drones);
+
+#endif
