2c202a2def845083a1acd41dd3949c6ea12fcfad
Aftertower8 <powierzaaleksander9@gmail.com>
2026-01-29 20:22:08 +0100

    poprawki w random_matrix.c, poprawiony Makefile

--- Makefile
+++ Makefile
@@ -1,6 +1,11 @@
-CFLAGS = -Wall -Wextra -pedantic
+FLAGS = -Wall -Wextra -pedantic
+
+all: pso generate
 
 pso: main.c map.c pso.c logger.c
 	cc $(FLAGS) $^ -o pso
+
+generate: random_matrix.c
+	cc $(FLAGS) $^ -o generate
 clean:
-	rm -f *.o pso
+	rm -f *.o pso generate
--- random_matrix.c
+++ random_matrix.c
@@ -1,55 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <time.h>
+#include <string.h>
 
-void generate(FILE *out, int w, int h, int min, int max){
+void generate(FILE *out, int w, int h, double min, double max){
     fprintf(out,"%d %d\n",w,h);
     for(int i=0;i<w;i++){
         for(int j=0;j<h;j++){
-            fprintf(out,"%d ", min + rand()%(max-min+1));
+            fprintf(out,"%lf ", min + (double)rand()/RAND_MAX*(max-min));
         }
         fprintf(out,"\n");
     }
 }
 
+void blad_arg(char* name){
+	printf("Uzycie: %s -f <plik_wyjsciowy> -w <szerokosc> -h <wysokosc> -min <min_val> -max <max_val>\n",name);
+}
+
 int main(int argc, char **argv){
     srand(time(NULL));
     char *flags[5]={"-f","-w","-h","-min", "-max"};
-    FILE *out=stdout;
-    int w=50, h=50, min=-100, max=100;
+    char *default_file = "random_matrix_generated.txt";
+    FILE *out=NULL;
+    int w=50, h=50;
+    double min=-100, max=100;
     for(int i=1; i<argc; i++){
         if(strcmp(argv[i],flags[0])==0 && i+1<argc){
             i++;
             out = fopen(argv[i],"w");
             if(out==NULL)
                 return EXIT_FAILURE;
-            i++;
+	    printf("Utworzono plik %s\n", argv[i]);
         }
         else if(strcmp(argv[i],flags[1])==0 && i+1<argc){
             i++;
             w=atoi(argv[i]);
-            i++;
         }
         else if(strcmp(argv[i],flags[2])==0 && i+1<argc){
             i++;
             h=atoi(argv[i]);
-            i++;
         }
         else if(strcmp(argv[i],flags[3])==0 && i+1<argc){
             i++;
-            min=atoi(argv[i]);
-            i++;
+            min=atof(argv[i]);
         }
         else if(strcmp(argv[i],flags[4])==0 && i+1<argc){
             i++;
-            max=atoi(argv[i]);
-            i++;
+            max=atof(argv[i]);
         }
         else{
+	    blad_arg(argv[0]);
             return EXIT_FAILURE;
         }
     }
+    if(out==NULL){
+	    out=fopen(default_file,"w");
+    	    printf("Utworzono plik %s\n",default_file);
+    }
+    if(w<1 || h<1){
+	    fprintf(stderr, "Blad! wysokosc lub szerokosc ujemne\n");
+	    return EXIT_FAILURE;
+    }
+    if(max<min){
+	    fprintf(stderr, "Blad! max mniejsze od min\n");
+	    return EXIT_FAILURE;
+    }
     generate(out,w,h,min,max);
+    fclose(out);
     return EXIT_SUCCESS;
 }
